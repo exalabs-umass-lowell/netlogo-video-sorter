0e5a9ce40b86bc91a4487c605cb4c64e14b6c4bf
Vultimate1 <34112804+Vultimate1@users.noreply.github.com>
2026-03-03 16:09:21 +0000

    Deploying to gh-pages from @ exalabs-umass-lowell/netlogo-video-sorter@8981d1c216a36edacd7b0021229d34c8147d4408 🚀

--- json-writer.sh
+++ json-writer.sh
@@ -13,9 +13,11 @@ for ((i=0; i<${#vids[@]}; i++)); do
 
    if [ "${file_ext: -4}" == ".gif" ]; then
        echo "$file_ext"
+       newf="${file_ext%.gif}.GIF"
+       mv "$file_ext" "$newf"
        json_str+="{\"name\":\"VID${count}\","
        json_str+=$'\n'
-       json_str+=" \"id\":\"/${vids[i]}\"}"
+       json_str+=" \"id\":\"/${newf}\"}"
        if (( count < vidnum )); then
            json_str+=","
            json_str+=$'\n'
@@ -25,12 +27,14 @@ for ((i=0; i<${#vids[@]}; i++)); do
        vidlist=("${file_ext}/"*)
        for ((j=0; j<${#vidlist[@]}; j++)); do
            if [ "${vidlist[j]: -4}" == ".gif" ]; then
-
+           f="${vidlist[j]}"
            count=$((count+1))
            echo $count
+           newf="${f%.gif}.GIF"
+           mv "$f" "$newf"
            json_str+="{\"name\":\"VID${count}\","
            json_str+=$'\n'
-           json_str+=" \"id\":\"/${vidlist[j]}\"}"
+           json_str+=" \"id\":\"/${newf}\"}"
            if (( count < vidnum )); then
                json_str+=","
                json_str+=$'\n'
